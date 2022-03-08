d5614217eeada6afd35a10ed8ae86c97642dcbe8
“Antoine <antoine.bendafi-schulmann@devinci.fr>
2022-03-08 17:51:37 +0100

    project page done

--- src/Controller/ProjectsController.php
+++ src/Controller/ProjectsController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Projects;
+use App\Repository\ProjectsRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProjectsController extends AbstractController
 {
+    /** @var ProjectsRepository */
+    private $projectsRepository;
+
+    public function __construct(EntityManagerInterface $objectManager)
+    {
+        $this->projectsRepository = $objectManager->getRepository(Projects::class);
+    }
+
     #[Route('/projects', name: 'projects')]
     public function index(): Response
     {
+        $project = $this->projectsRepository->findAll();
         return $this->render('projects/index.html.twig', [
-            'controller_name' => 'ProjectsController',
+            'projects' => $project,
         ]);
     }
 }
